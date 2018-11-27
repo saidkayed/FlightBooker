b39cb47f0a23ea0dcfb1c341c49036d68d3f2ae4
tobbe <tobbejj@hotmail.com>
2018-11-27 14:09:25 +0100

    jeg er picasso

--- BookerBackend/src/main/java/entity/Account.java
+++ BookerBackend/src/main/java/entity/Account.java
@@ -11,6 +11,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import org.mindrot.jbcrypt.BCrypt;
 
 /**
--- bookerfrontend/src/App.js
+++ bookerfrontend/src/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import Ticket from './Ticket';
 import { HashRouter as Router, Route, NavLink } from "react-router-dom";
-import login from './login';
+import ReactModal from 'react-modal';
+import "./loginmodal.css";
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+showModal: false
+    };
+  }
+  handleOpenModal = () => {
+    this.setState({ showModal: true });
+  }
+
+  handleCloseModal = () => {
+    this.setState({ showModal: false });
+  }
 
   render() {
     return (
@@ -14,10 +29,22 @@ class App extends Component {
            <NavLink exact to="/">Tickets</NavLink>
          </li>
          <li>
-        
+            <a onClick={this.handleOpenModal}>Log In</a>        
          </li>
        </ul>
        <Route exact path="/" component={Ticket}/>
+       <ReactModal className="ReactModal"
+           isOpen={this.state.showModal}
+           contentLabel="Minimal Modal Example">
+
+           <div className="ModalContent">
+           <h2 id="logintext">Log In</h2>
+           <input id="username" placeholder="username" type="username" name="username" required />
+           <input id="password" placeholder="password" type="password" name="password" required />
+           </div>
+           <a id="closebtn" onClick={this.handleCloseModal}>Close</a>
+
+        </ReactModal>
       </div>
       </Router>
     );
