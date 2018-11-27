c9b6f65caaba9602de1d7ed48230b73b14ea9035
CaptainVillain <kristian.krog@gmail.com>
2018-11-27 12:31:31 +0100

    Re-add deleted items

---
+++ bookerfrontend/src/Ticket.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import ShoppingCart from './ShoppingCart';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import Dropdown from 'react-dropdown'
+import 'react-dropdown/style.css'
+
+
+//HUSK at skrive "npm install react-dropdown" for dependency
+
+const options = ['Select Airline','THR','HEJ','TEST']
+const defaultOption = options[0]
+
+const URL = "http://localhost:8080/BookerBackend/api/ticket/alltickets/"
+
+export default class Ticket extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { names: [], sizePerPage: 10, page: 1, totalSize: 0, PSort: "", booked: []}
+    }
+
+    handleTableChange = async (type, props) => {
+        const { page, sizePerPage} = props;
+
+        const currentIndex = (page - 1) * sizePerPage;
+        const end = currentIndex + sizePerPage;
+
+
+        const URI = `${URL}?from=${currentIndex}&to=${end}` + this.state.PSort;
+        let p = await fetch(URI).then(res => {
+            const totalSize = Number(res.headers.get("X-Total-Count"));
+            if (totalSize) { this.setState({ totalSize }) }
+            return res.json()
+        });
+
+
+        const names = await p;
+        this.setState({ page, sizePerPage, names })
+
+
+    }
+
+    async componentDidMount() {
+        const { page, sizePerPage } = this.state
+        this.handleTableChange("didMount", { page, sizePerPage });
+    }
+
+
+    onSubmit = (ev) => {
+        ev.preventDefault();
+            this.setState({ PSort: "&Sort" })
+            this.forceUpdate(this.componentDidMount);
+    }
+
+
+    render() {
+        const { page, sizePerPage, totalSize } = this.state;
+        const columns = [{
+            dataField: 'airline',
+            text: 'Airline',
+        },
+        {
+            dataField: 'departure',
+            text: 'From',
+        },
+        {
+            dataField: 'desination',
+            text: 'Destination',
+        },
+        {
+            dataField: 'depTime',
+            text: 'Departure',
+        },
+        {
+            dataField: 'arrTime',
+            text: 'Arrival',
+        },
+        {
+            dataField: 'duration',
+            text: 'Duration',
+        },
+        {
+            dataField: 'price',
+            text: 'Price',
+        }, {
+            events: {
+                onClick: (e, column, columnIndex, row, rowIndex) => {
+
+                    this.setState({ booked: row })
+
+                },
+            },
+            formatter: () => {
+                return (
+
+                    <button>Book</button>
+
+                );
+
+            }
+        }]
+
+        return (
+
+            <div>
+                <ShoppingCart Book={this.state.booked} />
+                <form onSubmit={this.onSubmit}>
+                    <button>Price</button>
+                </form>
+
+                <Dropdown 
+                options={options} 
+                onChange={this._onSelect} 
+                value={defaultOption} 
+                placeholder="Select an option" />
+
+                <h2>Tickets</h2>
+                <BootstrapTable
+                    striped
+                    remote
+                    bootstrap4
+                    keyField='id'
+                    data={this.state.names}
+                    columns={columns}
+                    onTableChange={this.handleTableChange}
+                    pagination={paginationFactory({ page, sizePerPage, totalSize })}
+                />
+
+            </div>
+        )
+    }
+}
